7e29d58850bfa7eed075112fea10de8cd23cdeb5
gcsecsey <gcsecsey@gmail.com>
2019-04-24 00:04:49 +0200

    init

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => (
+  <NavStyles>
+    <Link href='/items'>
+      <a>Items</a>
+    </Link>
+    <Link href='/sell'>
+      <a>Sell</a>
+    </Link>
+    <Link href='/signup'>
+      <a>Signup</a>
+    </Link>
+    <Link href='/orders'>
+      <a>Orders</a>
+    </Link>
+    <Link href='/me'>
+      <a>Account</a>
+    </Link>
+  </NavStyles>
+);
+
+export default Nav;
