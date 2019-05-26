b36c25bfae7469fcb33413c7063c25e981a3b6fc
gcsecsey <gcsecsey@gmail.com>
2019-05-26 23:19:25 +0200

    17th video

--- sick-fits/frontend/pages/items.js
+++ sick-fits/frontend/pages/items.js
@@ -1,35 +1,3 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import Home from './index';
 
-const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
-			id
-			title
-			price
-			description
-			image
-			largeImage
-		}
-	}
-`;
-
-class Items extends Component {
-	render() {
-		return (
-			<div>
-				<p>Items!</p>
-				<Query query={ALL_ITEMS_QUERY}>
-					{({ data, error, loading }) => {
-						if (loading) return <p>Loading...</p>;
-						if (error) return <p>Error: {error.message}</p>;
-						return <p>I found {data.items.length} items!</p>;
-					}}
-				</Query>
-			</div>
-		);
-	}
-}
-
-export default Items;
+export default Home;
