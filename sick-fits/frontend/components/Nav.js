674aafcf7793aef18945f856b45913ffd5a57a3d
gcsecsey <gcsecsey@gmail.com>
2019-11-09 23:24:09 +0100

    sign out

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import User from './User';
+import Signout from './Signout';
 
 const Nav = () => (
-	<NavStyles>
-		<User>
-			{({ data: { me } }) => {
-				if (me) {
-					return <p>{me.name}</p>;
-				}
-				return null;
-			}}
-		</User>
-		<Link href='/items'>
-			<a>Shop</a>
-		</Link>
-		<Link href='/sell'>
-			<a>Sell</a>
-		</Link>
-		<Link href='/signup'>
-			<a>Signup</a>
-		</Link>
-		<Link href='/orders'>
-			<a>Orders</a>
-		</Link>
-		<Link href='/me'>
-			<a>Account</a>
-		</Link>
-	</NavStyles>
+	<User>
+		{({ data: { me } }) => (
+			<NavStyles>
+				<Link href='/items'>
+					<a>Shop</a>
+				</Link>
+				{me && (
+					<Fragment>
+						<Link href='/sell'>
+							<a>Sell</a>
+						</Link>
+						<Link href='/orders'>
+							<a>Orders</a>
+						</Link>
+						<Link href='/me'>
+							<a>Account</a>
+						</Link>
+						<Signout />
+					</Fragment>
+				)}
+				{!me && (
+					<Link href='/signup'>
+						<a>Signup</a>
+					</Link>
+				)}
+			</NavStyles>
+		)}
+	</User>
 );
 
 export default Nav;
