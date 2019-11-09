8dbf2b1f993b7a3aa5748534346c0cb652542409
gcsecsey <gcsecsey@gmail.com>
2019-11-09 22:36:03 +0100

    signup and show signed in user

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: '.env' });
 const cookieParser = require('cookie-parser');
 const createServer = require('./createServer');
 const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 const server = createServer();
 
@@ -9,6 +10,17 @@ const server = createServer();
 
 // cookieParser lets us access cookies in a formatted object rather than the cookie string
 server.express.use(cookieParser());
+
+// decode the JWT so we can get the userID
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 // TODO use express middleware to populate current user
 
